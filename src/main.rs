46dfe9213d4b141c20cd785f1e3ddc80c08dc0e3
YotioSoft <ytani0323@gmail.com>
2023-06-16 01:11:42 +0900

    sample 1

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+use texture_synthesis as ts;
+
+fn main() -> Result<(), ts::Error> {
+    //create a new session
+    let texsynth = ts::Session::builder()
+        //load a single example image
+        .add_example(&"imgs/1.png")
+        .build()?;
+
+    //generate an image
+    let generated = texsynth.run(None);
+
+    //save the image to the disk
+    generated.save("out/01.png")
+}
